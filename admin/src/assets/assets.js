3de82d6404bdb70f727e2ce59dc587b7002b3a92
Sujan Vishnumolakala <sujanv05@gmail.com>
2025-06-03 12:08:32 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -4,7 +4,7 @@ import order_icon from './order_icon.png'
 import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
-import foodlogo from './food_del_logo.png'
+import foodlogo from './food_logo.png'
 
 export const assets ={
     logo,
